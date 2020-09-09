4350152a1686c8f38613392c5b56dd53b0ec8107
Brian Gould <brian.gould@gmail.com>
2020-09-09 17:16:24 -0400

    Cleaning and experimenting with movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,6 +7,12 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    
+    //Limit the ship movement
+    //[SerializeField] private float screenWidthInUnits = 20f;
+    //[SerializeField] private float minX = -10f;
+    //[SerializeField] private float maxX = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +30,15 @@ public class Player : MonoBehaviour
         // Move Left and Right
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime;
         var newXPos = transform.position.x + deltaX * moveSpeed;
-
+        // MOve Up and Down
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime;
         var newYPos = transform.position.y + deltaY * moveSpeed;
+        
         transform.position = new Vector2(newXPos, newYPos);
+        
+        
+        //Vector2 shipPos = new Vector2(transform.position.x, transform.position.y);
+        //Debug.Log(shipPos);
         
         
     }

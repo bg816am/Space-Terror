8f8be68b03ab7bfee987fbf56e89f50ae166aa32
Brian Gould <brian.gould@gmail.com>
2020-09-12 00:19:08 -0400

    Lesson 100 done

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] private List<Transform> waypoints;
-    [SerializeField] private float moveSpeed = 2f;
+    WaveConfig _waveConfig;
+    List<Transform> waypoints;
     private int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = _waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
@@ -18,13 +19,16 @@ public class EnemyPathing : MonoBehaviour
     {
         Move();
     }
-
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this._waveConfig = waveConfig;
+    }
     private void Move()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = _waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
             if (transform.position == targetPosition)
             {
